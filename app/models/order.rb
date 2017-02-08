615df6c4b7c0a776811c6de01d9accc6f208c8ab
苗志良 <13585824737@163.com>
2017-02-08 22:11:43 +0800

    add order controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to :user
+  has_many :orders
+
+  validates :billing_name, presence: true
+  validates :billing_adress, presence: true
+  validates :shipping_name, presence: true
+  validates :shipping_adress, presence: true
 end
