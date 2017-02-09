e7a81524fe6c80aca127bdcb8e08ae28d254e3a5
苗志良 <13585824737@163.com>
2017-02-09 22:48:51 +0800

    add orders show function

--- db/migrate/20170208132237_create_orders.rb
+++ db/migrate/20170208132237_create_orders.rb
@@ -5,9 +5,9 @@ class CreateOrders < ActiveRecord::Migration[5.0]
         t.integer :total, default: 0
         t.integer :user_id
         t.string :billing_name
-        t.string :billing_adress
+        t.string :billing_address
         t.string :shipping_name
-        t.string :shipping_adress
+        t.string :shipping_address
       t.timestamps
     end
   end
